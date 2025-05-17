dba5c6131cdd91d5f09f095d076925fffad81cf0
vera bessonova <v2112365@gmail.com>
2025-05-17 16:59:56 +0300

    Tests fot task 2

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -50,4 +50,101 @@ public class OrderTest {
         String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+
+    // Тесты для второго задания невалидные данные
+    @Test
+    void formDataNotValidNameLatin() { //Поле Имя Латинские буквы
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = form.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Ivanov Ivan Ivanovich");
+        inputs.get(1).sendKeys("+71234567890");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void formDataNotValidNameSymbol() { //Поле Имя символы
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = form.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Ivanov !@45 Ivanovich");
+        inputs.get(1).sendKeys("+71234567890");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void formDataNotValidNameEmpty() { //Поле Имя пустое
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = form.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("");
+        inputs.get(1).sendKeys("+71234567890");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void formDataNotValidPhoneEmpty() { //Поле телефон пустое
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = form.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Иванов Иван Иванович");
+        inputs.get(1).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void formDataNotValidPhoneSymbol() { //Поле телефон символы
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = form.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Иванов Иван Иванович");
+        inputs.get(1).sendKeys("GGGПППпа112");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void formDataNotValidPhone12Number() { //Поле телефон более 11 цифр
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = form.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Иванов Иван Иванович");
+        inputs.get(1).sendKeys("+712345678901");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void formDataNotValidPhone9Number() { //Поле телефон менее 11 цифр
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = form.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Иванов Иван Иванович");
+        inputs.get(1).sendKeys("+7123456789");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void formDataNotClickCheckbox() { //Не установлена галочка согласия
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        List<WebElement> inputs = form.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Иванов Иван Иванович");
+        inputs.get(1).sendKeys("+71234567890");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']"));
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    }
 }
